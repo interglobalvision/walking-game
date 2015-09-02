93e9bad99ef017258304b2bc139c1cb614a0c999
mikey <michaelrayvon@gmail.com>
2015-09-01 19:39:14 -0500

    update bedside dialog

--- app/client/templates/animation/bedside.js
+++ app/client/templates/animation/bedside.js
@@ -1,7 +1,8 @@
 Template.bedside.onRendered(function () {
 
   var dialog = [
-      'Thank goodness you`re finally up.',
+      "Thank goodness you're finally up.  We are really running late!",
+      "Now what was your name again?",
       ];
 
   showText(dialog, 0, 0, 100);
