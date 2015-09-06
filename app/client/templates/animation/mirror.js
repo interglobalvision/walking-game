8f98babb6379a2f6fcca2a8dc477b9291c8a3190
mikey <michaelrayvon@gmail.com>
2015-09-06 18:55:36 -0500

    add video to mirror

--- app/client/templates/animation/mirror.js
+++ app/client/templates/animation/mirror.js
@@ -1,12 +1,55 @@
 Template.mirror.onRendered(function () {
+  var _this = this,
+    $stage = _this.$('#stage'),
+    $video = _this.$('#mirror-video'),
+    $videoView = _this.$('#mirror-video-viewport'),
+    video = document.getElementById("mirror-video"),
+    canvas = document.getElementById("mirror-canvas"),
+    ctx = canvas.getContext("2d"),
+    videoHeight = parseInt($video.attr('height')),
+    videoWidth = parseInt($video.attr('width')),
+    scaleHorz = $stage.width() / videoWidth,
+    scaleVert = $stage.height() / videoHeight,
+    scale = scaleHorz > scaleVert ? scaleHorz : scaleVert;
 
-  var dialog = [
-      "Thank goodness you're finally up.  We are really running late!",
-      "Now what was your name again?",
-      ];
+    $videoView.width($stage.width()).height($stage.height());
 
-  readDialog(dialog, 0, 0, function() {
-    $('#signup-form').show();
-  });
+    $video.width(scale * videoWidth).height(scale * videoHeight);
+
+    $videoView.scrollLeft(($video.width() - $stage.width()) / 2)
+      .scrollTop(($video.height() - $stage.height()) / 2);
+
+
+  navigator.getUserMedia = (navigator.getUserMedia ||
+                            navigator.webkitGetUserMedia ||
+                            navigator.mozGetUserMedia || 
+                            navigator.msGetUserMedia);
+
+  if (navigator.getUserMedia) {
+    // Request the camera.
+    navigator.getUserMedia(
+      // Constraints
+      {
+        video: true
+      },
+      // Success Callback
+      function(stream) {
+
+           video.src = window.URL.createObjectURL(stream);
+           video.onloadedmetadata = function(e) {
+             video.play();
+             ctx.drawImage(video, 0, 0, 640, 480);
+           };
+        },
+      // Error Callback
+      function(err) {
+        // Log the error to the console.
+        console.log('Error: ' + err);
+      }
+    );
+
+  } else {
+    alert('Sorry, no');
+  }
 
 });
