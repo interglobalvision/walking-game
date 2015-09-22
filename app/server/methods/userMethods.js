d54c24e47f1dd66839319608be6d9a101f7a750f
cas <casska@gmail.com>
2015-09-22 17:22:23 -0500

    fix check if user exists using default accounts function

--- app/server/methods/userMethods.js
+++ app/server/methods/userMethods.js
@@ -2,7 +2,7 @@ Meteor.methods({
   checkExistingUser: function(data) {
     check(data, {'username': String, 'email': String, password: String, });
 
-    var existingUser = Meteor.users.findOne({ emails: { $elemMatch: { address: data.email } } });
+    var existingUser = Accounts.findUserByEmail(data.email); //Meteor.users.findOne({ emails: { $elemMatch: { address: data.email } } });
 
     if (existingUser) {
 
