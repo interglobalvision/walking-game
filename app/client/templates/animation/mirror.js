a546aa575835793b0d793135cb28a4c06eaa58b5
mikey <michaelrayvon@gmail.com>
2015-09-22 13:58:31 -0500

    add method to save mirror image. add dialog and save button to mirror template. update button styling

--- app/client/templates/animation/mirror.js
+++ app/client/templates/animation/mirror.js
@@ -1,24 +1,35 @@
+var canvas, ctx, video, videoHeight, videoWidth;
+
 Template.mirror.onRendered(function () {
   var _this = this,
     $stage = _this.$('#stage'),
     $video = _this.$('#mirror-video'),
-    $videoView = _this.$('.mirror-video-viewport'),
-    video = document.getElementById("mirror-video"),
-    canvas = document.getElementById("mirror-canvas"),
-    ctx = canvas.getContext("2d"),
-    videoHeight = parseInt($video.attr('height')),
-    videoWidth = parseInt($video.attr('width')),
-    scaleHorz = $stage.width() / videoWidth,
+    $videoView = _this.$('#mirror-video-viewport');
+
+    video = document.getElementById("mirror-video");
+    videoHeight = parseInt($video.attr('height'));
+    videoWidth = parseInt($video.attr('width'));
+    
+  var scaleHorz = $stage.width() / videoWidth,
     scaleVert = $stage.height() / videoHeight,
-    scale = scaleHorz > scaleVert ? scaleHorz : scaleVert;
+    scale = scaleHorz > scaleVert ? scaleHorz : scaleVert,
+    dialog = [
+      "Goodness! Look at how "+word(adj)+" you look!",
+      "Let's remember this "+word(adj)+" face...",
+      ];
 
-    $videoView.width($stage.width()).height($stage.height());
+  $videoView.width($stage.width()).height($stage.height());
 
-    $video.width(scale * videoWidth).height(scale * videoHeight);
+  $video.width(scale * videoWidth).height(scale * videoHeight);
 
-    $videoView.scrollLeft(($video.width() - $stage.width()) / 2)
-      .scrollTop(($video.height() - $stage.height()) / 2);
+  $videoView.scrollLeft(($video.width() - $stage.width()) / 2)
+    .scrollTop(($video.height() - $stage.height()) / 2);
 
+  videoWidth = $video.width();
+  videoHeight = $video.height();
+
+  canvas = document.getElementById("mirror-canvas");
+  ctx = canvas.getContext("2d");
 
   navigator.getUserMedia = (navigator.getUserMedia ||
                             navigator.webkitGetUserMedia ||
@@ -35,12 +46,15 @@ Template.mirror.onRendered(function () {
       // Success Callback
       function(stream) {
 
-           video.src = window.URL.createObjectURL(stream);
-           video.onloadedmetadata = function(e) {
-             video.play();
-             ctx.drawImage(video, 0, 0, 640, 480);
-           };
-        },
+        video.src = window.URL.createObjectURL(stream);
+        video.onloadedmetadata = function(e) {
+          video.play();
+          
+          readDialog(dialog, 0, 0, function() {
+            $('#mirror-save').show();
+          });
+        };
+      },
       // Error Callback
       function(err) {
         // Log the error to the console.
@@ -53,3 +67,24 @@ Template.mirror.onRendered(function () {
   }
 
 });
+
+Template.mirror.events = {
+  'click #save-mirror-image': function(event){
+    event.preventDefault();
+
+    ctx.drawImage(video, 0, 0, 640, 320);
+
+    var userId = Meteor.userId(),
+      imageData = canvas.toDataURL();
+
+    console.log(imageData);
+
+    Meteor.call('saveMirrorImage', userId, imageData, function(error, result) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(result);
+      }
+    });
+  },
+};
