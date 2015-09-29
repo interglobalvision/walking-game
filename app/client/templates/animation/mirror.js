7bbc3a2b831b1fb5352d4aa7ee361c9cc4da184f
cas <casska@gmail.com>
2015-09-29 18:36:07 -0500

    fix mirror still

--- app/client/templates/animation/mirror.js
+++ app/client/templates/animation/mirror.js
@@ -1,66 +1,43 @@
-var canvas, ctx, video, videoHeight, videoWidth;
-
 Template.mirror.onRendered(function () {
-  var _this = this,
-    $stage = _this.$('#stage'),
-    $video = _this.$('#mirror-video'),
-    $videoView = _this.$('#mirror-video-viewport');
+  var _this = this;
 
-    video = document.getElementById("mirror-video");
-    videoHeight = parseInt($video.attr('height'));
-    videoWidth = parseInt($video.attr('width'));
-    
-  var scaleHorz = $stage.width() / videoWidth,
-    scaleVert = $stage.height() / videoHeight,
-    scale = scaleHorz > scaleVert ? scaleHorz : scaleVert,
-    dialog = [
-      "Goodness! Look at how "+word(adj)+" you look!",
-      "Let's remember this "+word(adj)+" face...",
-      ];
+  var video = document.getElementById("mirror-video");
+  var dialog = [
+    "Goodness! Look at how "+word(adj)+" you look!",
+    "Let's remember this "+word(adj)+" face...",
+  ];
 
-  $videoView.width($stage.width()).height($stage.height());
-
-  $video.width(scale * videoWidth).height(scale * videoHeight);
-
-  $videoView.scrollLeft(($video.width() - $stage.width()) / 2)
-    .scrollTop(($video.height() - $stage.height()) / 2);
-
-  videoWidth = $video.width();
-  videoHeight = $video.height();
-
-  canvas = document.getElementById("mirror-canvas");
-  ctx = canvas.getContext("2d");
-
-  navigator.getUserMedia = (navigator.getUserMedia ||
-                            navigator.webkitGetUserMedia ||
-                            navigator.mozGetUserMedia || 
-                            navigator.msGetUserMedia);
+  // Cross browser getUserMedia
+  navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia); 
 
   if (navigator.getUserMedia) {
     // Request the camera.
-    navigator.getUserMedia(
-      // Constraints
-      {
-        video: true
-      },
+    navigator.getUserMedia({
+      video: true
+    }, function(stream) {
       // Success Callback
-      function(stream) {
+      video.src = window.URL.createObjectURL(stream);
+      video.onloadedmetadata = function(e) {
 
-        video.src = window.URL.createObjectURL(stream);
-        video.onloadedmetadata = function(e) {
-          video.play();
-          
-          readDialog(dialog, 0, 0, function() {
-            $('#mirror-save').show();
-          });
-        };
-      },
+        video.play();
+
+        // Get Scales
+        var scaleWidth = window.innerWidth / video.videoWidth;
+        var scaleHeight = window.innerHeight / video.videoHeight;
+        var scale = scaleWidth > scaleHeight ? scaleWidth : scaleHeight;
+
+        // Apply scale to video
+        video.style.transform = "scale(" + scale + ")"
+
+        // Launch dialog
+        readDialog(dialog, 0, 0, function() {
+          $('#mirror-save').show();
+        });
+      };
+    }, function(err) {
       // Error Callback
-      function(err) {
-        // Log the error to the console.
-        console.log('Error: ' + err);
-      }
-    );
+      console.log('Error: ' + err);
+    });
 
   } else {
     alert('Sorry, no');
@@ -72,18 +49,29 @@ Template.mirror.events = {
   'click #save-mirror-image': function(event){
     event.preventDefault();
 
-    ctx.drawImage(video, 0, 0, 640, 320);
+    var canvas = document.getElementById("mirror-canvas");
+    var ctx = canvas.getContext("2d");
+    var video = document.getElementById("mirror-video");
+    var still = document.getElementById("mirror-still");
+    var videoHeight = canvas.height = video.videoHeight;
+    var videoWidth = canvas.width = video.videoWidth;
 
-    var userId = Meteor.userId(),
-      imageData = canvas.toDataURL();
+    ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+
+    var imageData = canvas.toDataURL();
+    still.style.backgroundImage = 'url(' + imageData + ')';
 
     console.log(imageData);
+
+    var userId = Meteor.userId();
 
     Meteor.call('saveMirrorImage', userId, imageData, function(error, result) {
       if (error) {
         console.log(error);
       } else {
         console.log(result);
+        $(video).fadeOut();
+        $(still).fadeIn();
       }
     });
   },
