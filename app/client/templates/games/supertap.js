ec09e6eac5f2b2b9f462866f210a474eabda58ec
cas <casska@gmail.com>
2015-09-30 18:46:24 -0500

    add supertap logic

---
+++ app/client/templates/games/supertap.js
@@ -0,0 +1,66 @@
+Template.supertap.helpers({
+
+  myHelper: function () {
+    //
+  }
+
+});
+
+Template.supertap.onCreated(function () {
+  var _this = this;
+  
+  Session.set('tapCount', 0);
+});
+
+Template.supertap.onRendered(function () {
+  var _this = this;
+
+});
+
+Template.supertap.onDestroyed(function () {
+  // Delete session vars
+  Session.set('tapCount', undefined)
+  Session.set('lastTap', undefined)
+  Session.set('endTime', undefined)
+  delete Session.keys.tapCount;
+  delete Session.keys.lastTap;
+  delete Session.keys.endTime;
+
+});
+
+Template.supertap.events({
+  'click #tap' : function (e) {
+
+    var date = new Date();
+
+    // Check if it's the first tap
+    if ( Session.equals('tapCount', 0) ) {
+      Session.set('endTime', date.getTime() + 30000 );
+      e.currentTarget.innerHTML = "Tap me MORE";
+    }
+
+    // Check if tapping is too slow
+    if ( date - Session.get('lastTap') > 300 ) {
+      // You lose
+      $('#tap').fadeOut();
+
+      // TODO: 
+      //  - show couch dialogs
+      //  - send back to map ?
+
+    } else if ( date.getTime() >= Session.get('endTime') ) {
+      // You win
+      $('#tap').fadeOut();
+
+      // TODO: 
+      //  - show couch dialogs
+      //  - send back to map ?
+    }
+
+    var count = Session.get('tapCount') + 1;
+    Session.set('tapCount', count);
+    Session.set('lastTap', date);
+    console.log(count);
+
+  }
+});
