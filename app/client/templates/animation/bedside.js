197a966c0d2ec35fb6aaeec6e496d918b48edf48
mikey <michaelrayvon@gmail.com>
2015-10-01 17:50:07 -0500

    add blackin to beginning of bedside scene

--- app/client/templates/animation/bedside.js
+++ app/client/templates/animation/bedside.js
@@ -1,12 +1,22 @@
 Template.bedside.onRendered(function () {
 
-  var dialog = [
+  var scene = new TimelineLite(),
+    $blackout = $('.blackout'),
+    dialog = [
       "Thank goodness you're finally up.  We are really running late!",
       "Now what was your name again?",
-      ];
+    ];
 
-  readDialog(dialog, 0, 0, function() {
+//Fade from black
+
+  scene.set($blackout, {display: 'block', opacity: 1});
+
+  scene.to($blackout, 3, {opacity: 0}, {ease:Bounce.easeIn});
+
+  scene.set($blackout, {display: 'none'});
+
+  scene.call( readDialog, [dialog, 0, 0, function() {
     $('#signup-form').show();
-  });
+  },]);
 
 });
