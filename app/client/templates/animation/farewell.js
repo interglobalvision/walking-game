97bcc93d9aa002337be529068b36b10dd297ab45
mikey <michaelrayvon@gmail.com>
2015-10-02 20:34:26 -0500

    add farewell template,  add living room room

---
+++ app/client/templates/animation/farewell.js
@@ -0,0 +1,25 @@
+Template.farewell.onRendered(function () {
+  var _this = this,
+    scene = new TimelineLite(),
+    $blackout = $('.blackout'),
+    dialog = [
+      "It's time to go...",
+    ];
+
+//Fade from black
+
+  scene.set($blackout, {display: 'block', opacity: 1,});
+
+  scene.to($blackout, 3, {opacity: 0,}, {ease:Bounce.easeIn,});
+
+  scene.set($blackout, {display: 'none',});
+
+  scene.call( readDialog, [dialog, 0, 0, function() {
+    console.log('/');
+  },]);
+ 
+});
+
+Template.bedside.events({
+  //
+});
