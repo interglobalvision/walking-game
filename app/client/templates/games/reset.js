c1e8b845571eb83d0882ca78610e0cfd24bd5b80
Patrick Best <patrickbest@patrickbest.com>
2015-10-08 15:28:58 -0500

    add template and basic logic

---
+++ app/client/templates/games/reset.js
@@ -0,0 +1,24 @@
+Template.reset.events({
+  'click #reset-yes' : function (event, template) {
+    var theHours = moment().format('HH');
+
+    theHours = parseInt(theHours);
+    Score.setNewGems(theHours);
+
+    console.log('You just got a bunch of gems :). This many: ', theHours);
+
+    // logic to reset progress [once we have progress state saved]
+
+    Router.go('map');
+
+  },
+
+  'click #reset-no' : function (event, template) {
+
+    console.log('ok fine');
+
+    Router.go('map');
+
+  },
+
+});
