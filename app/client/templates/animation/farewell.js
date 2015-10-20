7b8c9c2c522fc79093eea274154341f1d6a740ed
mikey <michaelrayvon@gmail.com>
2015-10-20 15:47:30 -0700

    update farewell animation

--- app/client/templates/animation/farewell.js
+++ app/client/templates/animation/farewell.js
@@ -3,6 +3,8 @@ Template.farewell.onRendered(function () {
     coach = new TimelineLite(),
     aunt = new TimelineLite(),
     compass = new TimelineLite(),
+    goodbye = new TimelineLite(),
+    blackout = new TimelineLite(),
     $blackout = $('.blackout'),
     $coach = _this.$('.coach-container'),
     $livingroom = _this.$('.livingroom'),
@@ -11,16 +13,21 @@ Template.farewell.onRendered(function () {
     moveCoachRight = new TweenMax.to($coach, 2, {left: '100%',}),
     moveRoomRight = new TweenMax.to($livingroom, 2, {left: '0%',}),
     dialog = [
-      "It's time to go...",
-      "But first your auntie has something for you...",
+      "It's time to hit the road...",
+      "Along the way you must complete minigame challenges!",
+      "That is the only way to become the best. Now say goodbye to your auntie...",
     ],
     dialog2 = [
-      "Oh deary i'm soo " + word(adj) + " to see you go... but you must go with your coach and train...",
+      "Oh my " + word(adj) + " " + word(noun) + " i'm soo " + word(adj) + " to see you go... but you must go with your coach and train...",
       "...train train train to be the BEST at walking in the world!",
       "But look ~ I have something special for you...",
     ],
     dialog3 = [
       "This is your compass...",
+      "It will tell you which way to walk to reach your next minigame challenge!",
+    ],
+    dialog4 = [
+      "Now go with your coach...and good luck!",
     ];
 
 //Fade from black
@@ -54,8 +61,26 @@ Template.farewell.onRendered(function () {
   compass.to($compassObj, 1, {width: '200px', height: '200px',}, {ease:Bounce.easeIn,});
 
   compass.call( readDialog, [dialog3, 0, 0, function() {
-    console.log('end');
+    goodbye.play();
   },]);
+
+  goodbye.pause();
+
+  goodbye.to($compass, 2, {opacity: 0,}, {ease:Bounce.easeIn,});
+
+  goodbye.to($compassObj, 1, {width: '0px', height: '0px',}, {ease:Bounce.easeIn,});
+
+  goodbye.call( readDialog, [dialog4, 0, 0, function() {
+    blackout.play();
+  },]);
+
+  blackout.pause();
+
+  blackout.set($blackout, {display: 'block', opacity: 0,});
+
+  blackout.to($blackout, 3, {opacity: 1,}, {ease:Bounce.easeIn,});
+
+  blackout.call(Router.go, ['/',]);
  
 });
 
