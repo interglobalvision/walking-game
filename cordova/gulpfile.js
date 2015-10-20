f5da09c253e00294bedb68f61e4ea18b511d39c0
cas <casska@gmail.com>
2015-10-20 18:06:07 -0500

    add gulp tasks

--- cordova/gulpfile.js
+++ cordova/gulpfile.js
@@ -5,18 +5,32 @@
 /* globals require */
 
 var gulp = require('gulp'),
-  jshint = require('gulp-jshint'),
-  jscs = require('gulp-jscs');
+  util = require('gulp-util'),
+  plumber = require('gulp-plumber'),
+  rename = require('gulp-rename'),
+  notify = require('gulp-notify'),
+  util = require('gulp-util'),
+  concat = require('gulp-concat'),
 
+  jshint = require('gulp-jshint'),
+  jscs = require('gulp-jscs'),
+
+  stylus = require('gulp-stylus'),
+  autoprefixer = require('gulp-autoprefixer'),
+  minifycss = require('gulp-minify-css');
+
+function errorNotify(error){
+  notify.onError("Error: <%= error.message %>");
+  util.log(util.colors.red('Error'), error.message);
+}
 /**
  * @task JavaScript lint.
  *   Runs JSCS and JSHint on server, client, lib, and gulp files.
  */
 gulp.task('lintjs', function () {
   return gulp.src([
-    'gulpfile.js',
-    'games/**/*.js',
-    'scenes/**/*.js',
+    './www/games/**/*.js',
+    './www/scenes/**/*.js',
   ])
   .pipe(jshint())
   .pipe(jshint.reporter('default'))
@@ -24,15 +38,39 @@ gulp.task('lintjs', function () {
 });
 
 /**
+ * @task Stylus compile
+ *   Compiles .styl files into .css 
+ *   Minifies .css into a single .min.css file
+ */
+gulp.task('style', function() {
+  return gulp.src('./www/css/**.styl')
+  .pipe(plumber())
+  .pipe(stylus())
+  .on('error', errorNotify)
+  .pipe(autoprefixer())
+  .on('error', errorNotify)
+  .pipe(concat('main.css'))
+  .pipe(gulp.dest('./www/css'))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(minifycss())
+  .on('error', errorNotify)
+  .pipe(gulp.dest('./www/css'))
+  .pipe(notify({message: 'Style task complete'}));
+});
+
+/**
  * @task JavaScript/JSON watch.
- *   Watches changes on relevant js and json files and reports accordingly.
+ *   Watches changes on relevant fils and runs proper tasks
  */
 gulp.task('watch', function () {
   gulp.watch([
-    'gulpfile.js',
-    'games/**/*.js',
-    'scenes/**/*.js',
+    './www/games/**/*.js',
+    './www/scenes/**/*.js',
   ], ['lintjs',]);
+
+  gulp.watch([
+    './www/css/**/*.styl',
+  ], ['style',]);
 });
 
 gulp.task('default', ['watch',]);
