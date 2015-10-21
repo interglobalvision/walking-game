818752dbc49a8b176d01c001de9d335b9b9a8b12
Patrick Best <patrickbest@patrickbest.com>
2015-10-20 19:26:20 -0500

    add complied files

--- cordova/www/js/index.js
+++ cordova/www/js/index.js
@@ -1,3 +1,64 @@
+Game = {
+  minigames: [
+    'supertap',
+    'reset',
+    'photocolor',
+  ],
+
+  resetProgress: function() {
+    window.localStorage.setItem('progress', 0);
+  },
+
+  gameComplete: function() {
+    var currentProgress = parseInt(window.localStorage.getItem('progress'));
+
+    window.localStorage.setItem('progress', (currentProgress + 1));
+    Router.go('/');
+  },
+
+  nextMinigame: function() {
+    var currentProgress = parseInt(window.localStorage.getItem('progress'));
+
+    Router.go('/games/' + this.minigames[currentProgress]);
+  },
+
+  getProgressPercent: function() {
+    var currentProgress = parseInt(window.localStorage.getItem('progress'));
+
+    return currentProgress / this.minigames.length;
+  },
+
+  getPoints: function() {
+    return window.localStorage.getItem('points');
+  },
+
+  setNewPoints: function(points) {
+    var points = parseInt(points);
+    var currentPoints = parseInt(window.localStorage.getItem('points'));
+    var currentGems = parseInt(window.localStorage.getItem('gems'));
+
+    if (points > 0) {
+      var modifier = (Math.log(currentGems) + 1);
+      var modifiedPoints = Math.round((points * modifier));
+
+      window.localStorage.setItem('points', (currentPoints + modifiedPoints));
+    } else {
+      window.localStorage.setItem('points', (currentPoints + points));
+    }
+  },
+
+  getGems: function() {
+    return window.localStorage.getItem('gems');
+  },
+
+  setNewGems: function(gems) {
+    var gems = parseInt(gems);
+    var currentGems = parseInt(window.localStorage.getItem('gems'));
+
+    window.localStorage.setItem('gems', (currentGems + gems));
+  },
+
+};
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -46,3 +107,8 @@ var app = {
 };
 
 app.initialize();
+Router = {
+  go: function(url) {
+    $(location).attr('href', url);
+  },
+}
