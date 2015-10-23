b67228f87f29eae9eb9993ba2c5de858cbfd9202
cas <casska@gmail.com>
2015-10-22 19:39:26 -0500

    fix router src

--- cordova/www/js/src/router.js
+++ cordova/www/js/src/router.js
@@ -1,5 +1,14 @@
 Router = {
+  init: function() {
+    var _this = this;
+
+    var regex =  /(.+?(?:www))/;
+    _this.basePath = regex.exec(window.location.href);
+  },
   go: function(url) {
-    $(location).attr('href', url);
+    var _this = this;
+
+    window.location = _this.basePath[0] + url + 'index.html';
   },
 }
+Router.init();
