4e19f9592b5717a8c2f11049aa802422f2cba4fb
cas <casska@gmail.com>
2015-10-23 17:44:05 -0500

    fix Dialog declaration

--- cordova/www/js/index.js
+++ cordova/www/js/index.js
@@ -198,99 +198,97 @@ Utilities.Color = {
     return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255),];
   }
 };
-$(document).ready( function() {
-  Utilities.Dialog = {
-    $target: $('.text-box-dialog'),
-    $parent: $('#dialog'),
-    interval: 66,
+Utilities.Dialog = {
+  $target: $('.text-box-dialog'),
+  $parent: $('#dialog'),
+  interval: 66,
 
-    arrayIndex: 0,
+  arrayIndex: 0,
 
-    lineIndex: 0,
-    lineTimer: 0,
+  lineIndex: 0,
+  lineTimer: 0,
 
-    read: function(dialogArray, callback) {
+  read: function(dialogArray, callback) {
 
-      var _this = this;
+    var _this = this;
 
-      _this.$parent = $('#dialog');
-      _this.$target = $('.text-box-dialog');
+    _this.$parent = $('#dialog');
+    _this.$target = $('.text-box-dialog');
 
-      _this.dialogArray = dialogArray;
-      _this.arrayIndex = 0;
-      _this.callback = callback;
+    _this.dialogArray = dialogArray;
+    _this.arrayIndex = 0;
+    _this.callback = callback;
 
-      _this.$parent.show();
+    _this.$parent.show();
 
-      _this.$parent.off('click.dialogRead').on({
-        'click.dialogRead': function() {
-          if (_this.lineTimer > 0) {
-            _this.skipLine();
-          } else {
-            if (_this.arrayIndex === (_this.dialogArray.length - 1)) {
-              _this.finish();
-            } else {
-              _this.arrayIndex++;
-              _this.readLine();
-            }
-          }
-        },
-      });
-
-      _this.readLine();
-
-    },
-
-    readLine: function() {
-      var _this = this;
-      var dialogLine = _this.dialogArray[_this.arrayIndex];
-
-      _this.lineIndex = 0;
-      _this.$target.html('');
-      _this.lineTimer = setInterval(function() {
-
-        if (_this.lineIndex < dialogLine.length) {
-
-          _this.$target.append(dialogLine[_this.lineIndex]);
-          _this.lineIndex++;
-
+    _this.$parent.off('click.dialogRead').on({
+      'click.dialogRead': function() {
+        if (_this.lineTimer > 0) {
+          _this.skipLine();
         } else {
-
-          _this.clearLineInterval();
-          _this.$target.append('<a class="text-box-next">&rarr;</a>');
-
+          if (_this.arrayIndex === (_this.dialogArray.length - 1)) {
+            _this.finish();
+          } else {
+            _this.arrayIndex++;
+            _this.readLine();
+          }
         }
+      },
+    });
 
-      }, _this.interval);
-    },
+    _this.readLine();
 
-    clearLineInterval: function() {
-      var _this = this;
+  },
 
-      clearInterval(_this.lineTimer);
-      _this.lineTimer = 0;
-    },
+  readLine: function() {
+    var _this = this;
+    var dialogLine = _this.dialogArray[_this.arrayIndex];
 
-    skipLine: function() {
-      var _this = this;
+    _this.lineIndex = 0;
+    _this.$target.html('');
+    _this.lineTimer = setInterval(function() {
 
-      _this.clearLineInterval();
-      _this.$target.html(_this.dialogArray[_this.arrayIndex]);
-      _this.$target.append('<a class="text-box-next">&rarr;</a>');
+      if (_this.lineIndex < dialogLine.length) {
 
-    },
+        _this.$target.append(dialogLine[_this.lineIndex]);
+        _this.lineIndex++;
 
-    finish: function() {
-      var _this = this;
+      } else {
 
-      _this.$parent.hide();
-      _this.$target.html('');
+        _this.clearLineInterval();
+        _this.$target.append('<a class="text-box-next">&rarr;</a>');
 
-      _this.callback();
-    },
+      }
 
-  };
-});
+    }, _this.interval);
+  },
+
+  clearLineInterval: function() {
+    var _this = this;
+
+    clearInterval(_this.lineTimer);
+    _this.lineTimer = 0;
+  },
+
+  skipLine: function() {
+    var _this = this;
+
+    _this.clearLineInterval();
+    _this.$target.html(_this.dialogArray[_this.arrayIndex]);
+    _this.$target.append('<a class="text-box-next">&rarr;</a>');
+
+  },
+
+  finish: function() {
+    var _this = this;
+
+    _this.$parent.hide();
+    _this.$target.html('');
+
+    _this.callback();
+  },
+
+};
 
 Utilities.Word = {
   adjs: [],
