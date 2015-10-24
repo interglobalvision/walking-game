113ee6dc122ab6ba56787bbbccbd315fcb44292c
Patrick Best <patrickbest@patrickbest.com>
2015-10-23 19:55:48 -0500

    fix router for browser too

--- cordova/www/js/src/router.js
+++ cordova/www/js/src/router.js
@@ -4,11 +4,21 @@ Router = {
 
     var regex =  /(.+?(?:www))/;
     _this.basePath = regex.exec(window.location.href);
+
+    if (window.cordova) {
+      _this.isBrowser = false;
+    } else {
+      _this.isBrowser = true;
+    }
   },
   go: function(url) {
     var _this = this;
 
-    window.location = _this.basePath[0] + url + 'index.html';
+    if (_this.isBrowser) {
+      window.location = url;
+    } else {
+      window.location = _this.basePath[0] + url + 'index.html';
+    }
   },
 }
 Router.init();
