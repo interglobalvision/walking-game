ebf6cd7da64314ca4501e871f83d5c0cb76c334b
mikey <michaelrayvon@gmail.com>
2015-10-28 18:44:42 -0700

    update wakeup animation structure.  update wakeup scene selectors

--- cordova/www/scenes/wakeup/wakeup.js
+++ cordova/www/scenes/wakeup/wakeup.js
@@ -3,61 +3,30 @@ var Wakeup = {
     var _this = this;
 
     var scene = new TimelineLite(),
-      blackIn = new TimelineLite(),
-      openEyes = new TimelineLite(),
-      blackOut = new TimelineLite(),
+      scene2 = new TimelineLite(),
       $blackout = $('#blackout'),
-      $coach = $('.coach-angry'),
-      $bed = $('.bedroom-bed'),
-      $wall = $('.bedroom-wall'),
-      $furniture = $('.bedroom-furniture'),
       dialog = [
         'Goodness!  I have been screaming my ' + Utilities.Word.getAdj() + ' ' + Utilities.Word.getNoun() + ' off trying to get you out of bed!',
         'You ' + Utilities.Word.getAdj() + ' ' + Utilities.Word.getNoun() + '!  Youre ' + Utilities.Word.getAdj(true) + ' ' + Utilities.Word.getNoun() + '!',
         'And your bedroom smells like ' + Utilities.Word.getAdj() + ' ' + Utilities.Word.getNoun() + ' and ' + Utilities.Word.getAdj() + ' ' + Utilities.Word.getNoun() + '!',
       ];
 
-    $blackout.css({
-      display: 'none',
+    scene.set($blackout, {opacity: 0,});
+
+    scene.call(function() {
+      Utilities.Dialog.read(dialog, function() {
+        scene2.play()
+      });
     });
 
-    //Scene timeline
-    scene.play();
+    scene2.pause();
 
-    //add Fade from black to scene timeline
-    scene.add(blackIn);
+    scene2.set($blackout, {opacity: 1,});
 
-    //add text-box wakeup dialog to scene <timeline></timeline>
-    scene.add(function() {
-
-      Utilities.Dialog.read(dialog, function() {
-
-        $blackout.css({
-          display: 'none',
-        });
-        Router.go('/scenes/bedside/');
-
-      });
-
+    scene2.call(function() {
+      Router.go('/scenes/bedside/');
     });
 
-/*
-    scene.add(TweenLite.delayedCall(0, function() {
-
-      Utilities.Dialog.read(dialog, function() {
-
-        blackOut.set($blackout, {display: 'block',});
-        blackOut.to($blackout, 3, {opacity: 1,}, {ease:Bounce.easeIn,});
-        blackOut.call(function() {
-
-          Router.go('/scenes/bedside/');
-
-        });
-
-      });
-
-    }));
-*/
   },
 };
 
