19d0a6fd8194631a832488a2c72908aa926452c2
Patrick Best <patrickbest@patrickbest.com>
2016-01-29 14:49:30 -0600

    add built files

--- cordova/www/js/index.js
+++ cordova/www/js/index.js
@@ -28,7 +28,7 @@ Compass = {
   */
   minDistance: 0.0025, // in radians
   maxDistance: 0.0028, // in radians
-  thresholdRadius: 0.300, // in Km
+  destinyThresholdRadius: 0.300, // in Km
 
   totalDistance: 0,
 
@@ -126,27 +126,26 @@ Compass = {
     // Check distance in Km between position and destiny
     var distanceToDestiny = _this.getDistanceInKm(_this.position, _this.destiny);
 
-    var distanceFromGoal = ( (distanceToDestiny - _this.thresholdRadius) * 1000 ) / _this.totalDistance;// - _this.thresholdRadius;
-
-    // distanceFromGoal is abstract value of users distance from goal radius.
-    // distanceFromGoal begins at 1000.
-    // distanceFromGoal decreases as user moves closer to radius
-    // and increases as user moves further.
+    // distanceFromGoal is a percentage value describing how far you are from the destiny [meaning the threshold of destiny and the moment the next game triggers].
+    // This is based on your location from the moment the destiny is created.
+    // So at 100[%] you have just generated a new destiny. At 0[%] you are at destiny.
+    // This value can be larger than 100 if you move in the wrong direction.
+    var distanceFromGoal = ( (distanceToDestiny - _this.destinyThresholdRadius) * 100 ) / _this.totalDistance;
 
     // progressToGoal is users progress toward goal radius.
-    // distanceFromGoal is subtracted from 1000
-    // to give us minimum 0, max 1000 (goal is reached).
-    var progressToGoal = 1000 - distanceFromGoal;
+    // this is the inverse of distanceFromGoal
+    // 0[%] is moment of destiny creation and 100[%] is at destiny
+    var progressToGoal = 100 - distanceFromGoal;
 
     // progressToGoal is multiplied to a thousandth decimal point of 75
     // to use as pecentage of 75% when moving the map floor gradient.
     // 75% is the full Y axis translation of the gradient
-    var mapFloorPos = progressToGoal * 0.001 * 75;
+    var mapFloorPos = progressToGoal * 0.75;
 
     // progressToGoal is multiplied to a thousandth decimal point
     // to use as scale of the map Goal object.
-    // 1.000 is the object at full scale (goal is reached).
-    var mapGoalScale = progressToGoal * 0.001;
+    // 1.00 is the object at full scale (goal is reached).
+    var mapGoalScale = progressToGoal * 0.01;
 
     // if mapFloorPos is less than 0, we set it to 0
     // this keeps the floor from sliding off screen
@@ -154,10 +153,10 @@ Compass = {
       mapFloorPos = 0;
     }
 
-    // if mapGoalScale is less than 0.001, we set it to 0.001
+    // if mapGoalScale is less than 0.01, we set it to 0.01
     // goal object from disappearing entirely or going negative scale
-    if (mapGoalScale < 0.001) {
-      mapGoalScale = 0.001;
+    if (mapGoalScale < 0.01) {
+      mapGoalScale = 0.01;
     }
 
     _this.$mapFloor.css({
@@ -170,10 +169,10 @@ Compass = {
       'transform': 'scale(' + mapGoalScale + ')',
     });
 
-    if( distanceToDestiny < _this.thresholdRadius ) {
+    if (distanceToDestiny < _this.destinyThresholdRadius) {
       _this.stop();
       Game.nextMinigame();
-    } 
+    }
   },
 
   updateOrientation: function(orientation) {
@@ -185,7 +184,7 @@ Compass = {
     var compensationAngle = _this.getAngle( _this.reference, _this.position, _this.destiny);
 
     // If destiny is at West of origin
-    if( _this.position.lng > _this.destiny.lng ) {
+    if (_this.position.lng > _this.destiny.lng) {
       compensationAngle = 360 - compensationAngle;
     }
 
@@ -193,8 +192,8 @@ Compass = {
 
     // When the compass is pointed 70deg (+ or -) from 0 (top),
     // the arrow points offscreen.  So we get a percent of 70
-    // to position the goal object with the arrow 
-    var goalPos = angle / 0.7; 
+    // to position the goal object with the arrow
+    var goalPos = angle / 0.7;
 
     _this.$mapGoalContainer.css({
       '-webkit-transform': 'translateX(' + goalPos + '%)',
@@ -217,19 +216,19 @@ Compass = {
       var hour = now.getHours(),
         skyColor;
 
-      if ( hour > 4 && hour < 10 ) { 
+      if (hour > 4 && hour < 10) {
 
         skyColor = 'rgb(100, 160, 255)'; // Morning 5 - 9
 
-      } else if ( hour > 9 && hour < 17 ) { 
+      } else if (hour > 9 && hour < 17) {
 
         skyColor = 'rgb(0, 120, 255)'; // Day 10 - 16
 
-      } else if ( hour > 16 &&  hour < 22 ) { 
+      } else if (hour > 16 &&  hour < 22) {
 
         skyColor = 'rgb(10, 40, 95)'; // Evening 17 - 21
 
-      } else { 
+      } else {
 
         skyColor = 'rgb(0, 20, 60)'; // Night 22 - 4
 
@@ -261,7 +260,7 @@ Compass = {
     });
 
     // Start orientation compass
-    
+
     // if cordova
     if (navigator.userAgent.match(/(iPhone|iPod|Android)/)) {
       _this.watchId.orientation = navigator.compass.watchHeading( function(heading) {
@@ -312,7 +311,7 @@ Compass = {
         _this.totalDistance = _this.getDistanceInKm({
           lat: pos.latitude,
           lng: pos.longitude
-        }, _this.destiny) - _this.thresholdRadius;
+        }, _this.destiny) - _this.destinyThresholdRadius;
 
         // Set current position
         _this.updatePosition({
