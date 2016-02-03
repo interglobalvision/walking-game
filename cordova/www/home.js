f2f2282ed88e68ddd3610ab1ee02344c3a02086c
cas <casska@gmail.com>
2016-02-02 18:13:49 -0600

    add share to FB button must run: cordova plugin add cordova-plugin-x-socialsharing && cordova prepare

--- cordova/www/home.js
+++ cordova/www/home.js
@@ -30,3 +30,15 @@ Home = {
 document.addEventListener('deviceready', function() {
   Home.init();
 }, false);
+
+// Share event listeners
+$('.share-fb').click(function(e){
+  e.preventDefault();
+
+  var score = Game.getPoints();
+
+  window.plugins.socialsharing.shareViaFacebook('WOOAAAAHH! U HAVE AN AWESOME SCORe 0F' + score + ' POIIINTSSS BRAAAHHH', 'http://puu.sh/mTFtM/242a0fa967.png', 'http://interglobal.vision/', function() {
+    console.log('share ok');
+  }, function(errormsg){
+    alert(errormsg)})
+});
