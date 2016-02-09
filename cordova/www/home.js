5494b2200a58611ab017e8d45266f4a7ee5cce6c
cas <casska@gmail.com>
2016-02-09 10:29:35 -0600

    add End Map button (its for dev purposes)

--- cordova/www/home.js
+++ cordova/www/home.js
@@ -6,11 +6,18 @@ Home = {
     var _this = this;
 
     _this.$compassContainer = $("#stage");
+    _this.$compassEnd = $("#end-compass");
 
     // check if fresh game
     if( !Game.getUsername() ) {
       Router.go('/scenes/wakeup/');
     } 
+
+    _this.$compassEnd.click( function(event) {
+      event.preventDefault();
+
+      Compass.stop();
+    });
 
     _this.$compassContainer.fadeIn();
 
