a99fd8be93760807f728476bd82916ffa7f20655
Michael Ray-Von <mikey@interglobal.vision>
2016-03-21 20:09:43 -0600

    update menu styling. add toggle function

--- cordova/www/js/src/menu.js
+++ cordova/www/js/src/menu.js
@@ -1,3 +1,21 @@
 Menu = {
+	$menuBubble: $('#map-menu-bubble'),
+	$menuButton: $('#map-menu-button'),
+	menuState: 0, //0 is closed, 1 is open
+
+	toggleMenu: function() {
+		//functionality to open and close menu
+		var _this = this;
+
+		_this.$menuBubble.toggle("fast");
+	},
+
+	init: function() {
+		var _this = this;
+
+		_this.$menuButton.on('click', function() {
+			_this.toggleMenu();
+		}); 
+	}
 
 };
