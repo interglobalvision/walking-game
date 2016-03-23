22621aa98cacfb724fbf4315116f1e56e8f876cb
Patrick Best <patrickbest@patrickbest.com>
2016-03-22 18:40:39 -0600

    trigger sharing on menu item click

--- cordova/www/pages/compass/compass.js
+++ cordova/www/pages/compass/compass.js
@@ -2,16 +2,19 @@ Home = {
   init: function() {
     var _this = this;
 
-    $("#end-compass").click( function(event) {
+    $('#end-compass').click( function(event) {
       event.preventDefault();
-
       Compass.stop();
     });
 
+    $('#menu-share').click(function() {
+      Game.shareWithOptions();
+    });
+
     // Check if fresh game
-    if( !Game.getUsername() ) {
+    if (!Game.getUsername()) {
       Router.go('/scenes/wakeup/');
-    } else { 
+    } else {
       Compass.init();
       Menu.init();
     }
