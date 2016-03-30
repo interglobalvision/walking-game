27e49d916f88a30f551d640c28a8d7af98f14eae
Michael Ray-Von <mikey@interglobal.vision>
2016-03-29 22:21:39 -0600

    add current world to letsgo dialog

--- cordova/www/scenes/letsgo/letsgo.js
+++ cordova/www/scenes/letsgo/letsgo.js
@@ -6,7 +6,7 @@ var Letsgo = {
       "Follow the compass that your Auntie gave you, and find the golden flag...",
       "Each time you reach a flag - I will give you a MINIGAME CHALLENGE!!!",
       "Complete 3 minigame challenges to move to the next world...AND gain a new rank!!",
-      "To begin you are ... " + Game.getRank(),
+      "To begin you are ... " + Game.getRank() + " in the " + Game.getWorldName ".",
       "If you get lost on your way, just tap me at the bottom right...",
       "OK! LETS GO!",
   ],
