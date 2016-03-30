8f77b59681908efc718b3160e28a346c2b92fd5c
Michael Ray-Von <mikey@interglobal.vision>
2016-03-29 21:44:45 -0600

    add shiny coach sprite. update lets go scene. add letsgo dialog. add router to map.

--- cordova/www/scenes/letsgo/letsgo.js
+++ cordova/www/scenes/letsgo/letsgo.js
@@ -1,7 +1,14 @@
 var Letsgo = {
   $blackout: $('#blackout'),
   dialog: [
-      "Lets go!",
+      "Great " + Game.getUsername() + "!! We're on our way...",
+      "And it's time to go on our first walk...",
+      "Follow the compass that your Auntie gave you, and find the golden flag...",
+      "Each time you reach a flag - I will give you a MINIGAME CHALLENGE!!!",
+      "Complete 3 minigame challenges to move to the next world...AND gain a new rank!!",
+      "To begin you are ... " + Game.getRank(),
+      "If you get lost on your way, just tap me at the bottom right...",
+      "OK! LETS GO!",
   ],
 
   init: function() {
@@ -18,10 +25,22 @@ var Letsgo = {
   partOne: function() {
     var _this = this;
 
+    Utilities.Dialog.read(_this.dialog, function() {
+
+      _this.partTwo();
+
+    });
+
   },
 
   partTwo: function() {
     var _this = this;
+
+    _this.$blackout.animate({'opacity': 1,}, 2000, 'linear', function() { //fade to black
+
+      Router.go('/pages/compass/'); //go to map
+
+    });
 
   },
 
