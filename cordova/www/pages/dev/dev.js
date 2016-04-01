c9b01bf191fbe6cc510f72dd3f2e4eb28814ecfa
Patrick Best <patrickbest@patrickbest.com>
2016-03-31 21:07:47 -0600

    create dev page for dev menu

---
+++ cordova/www/pages/dev/dev.js
@@ -0,0 +1,16 @@
+Dev = {
+  init: function() {
+    var _this = this;
+
+    $('#play-next').click( function(event) {
+      event.preventDefault();
+
+      Router.go('/pages/compass/');
+    });
+
+  },
+}
+
+document.addEventListener('deviceready', function() {
+  Dev.init();
+}, false);
