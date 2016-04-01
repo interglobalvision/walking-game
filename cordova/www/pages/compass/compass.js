fd072619bf66f12720ad4d3e4db7fd5e17eae66b
Patrick Best <patrickbest@patrickbest.com>
2016-03-31 21:07:54 -0600

    link the flow up

--- cordova/www/pages/compass/compass.js
+++ cordova/www/pages/compass/compass.js
@@ -6,14 +6,6 @@ Home = {
       Game.shareWithOptions();
     });
 
-    // Check if fresh game
-    if (!Game.getUsername()) {
-      Router.go('/scenes/wakeup/');
-    } else {
-      Compass.init();
-      Menu.init();
-    }
-
   },
 }
 
