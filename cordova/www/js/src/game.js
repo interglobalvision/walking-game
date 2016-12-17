73f6e159c09e3ba7122f2f328ce21896e3135e22
cas <cas@interglobal.vision>
2016-12-16 18:02:24 -0600

    add getStepsPot()

--- cordova/www/js/src/game.js
+++ cordova/www/js/src/game.js
@@ -95,6 +95,10 @@ Game = {
     window.localStorage.setItem('stepsPot', pot);
   },
 
+  getStepsPot: function() {
+    return window.localStorage.getItem('stepsPot') ? parseFloat( window.localStorage.getItem('stepsPot') ) : 0;
+  },
+
   getTotalDistance: function() {
     return window.localStorage.getItem('distance') ? parseFloat( window.localStorage.getItem('distance') ) : 0;
   },
