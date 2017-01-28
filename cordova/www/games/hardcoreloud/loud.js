7c4e7d36f234da061cf26502877938473930bb64
Patrick Best <patrickbest@patrickbest.com>
2017-01-27 19:45:17 -0600

    add game logic

--- cordova/www/games/hardcoreloud/loud.js
+++ cordova/www/games/hardcoreloud/loud.js
@@ -1,34 +1,73 @@
 var Loud = {
   $blackout: $('#blackout'),
+  $loudBar: $('#loud-bar'),
+
+  failTime: 5000,
+  winTime: 200,
+  winTheshold: .85,
+
+  introDialog: [
+    "This one is a lung tester. I need you to scream. Really loudly!",
+    "Get ready and throw that shout",
+  ],
+  tryAgainDialog: [
+    "...too quiet...I can't hear you...",
+  ],
+  loseDialog: [
+    "no more than a whimper",
+  ],
+  winDialog: [
+    "PUM! BOOM! BANG! Nice one",
+  ],
+
   init:  function() {
     var _this = this;
 
-    _this.$loudBar = $('#loud-bar');
+    _this.$blackout.animate({'opacity': 0,}, 1000, 'linear');
+
+    Utilities.Dialog.read(_this.introDialog, function() {
+      _this.startGame();
+    });
+
+  },
+
+  startGame: function() {
+    var _this = this;
 
     var src = 'myrecording.wav';
+
     _this.mediaRec = new Media(src,
       // success callback
-      function() {
-        console.log("recordAudio():Audio Success");
-      },
+      function(event) {
 
+        console.log(event);
+
+      },
       // error callback
       function(err) {
         console.log("recordAudio():Audio Error: "+ err.code);
         console.log(err);
-      });
+        WalkingError.unsupported('microphone access');
+    },
+      function(event) {
+
+        console.log(event);
+
+      }
+    );
 
     // Record audio
     _this.mediaRec.startRecord();
 
+    _this.paused = false;
+
     // Pause Recording after 5 seconds
-    setTimeout(function() {
-      _this.mediaRec.pauseRecord();
-    }, 10000);
+    _this.failTimeout = window.setTimeout(function() {
+      _this.fail();
+    }, _this.failTime);
 
     requestAnimationFrame(_this.draw.bind(_this));
 
-    _this.$blackout.animate({'opacity': 0,}, 1000, 'linear');
   },
 
   draw: function() {
@@ -36,18 +75,82 @@ var Loud = {
 
     if (_this.mediaRec) {
       _this.mediaRec.getCurrentAmplitude(function(value) {
-        console.log(value);
         _this.$loudBar.height(value * 100 + '%');
+
+        if (value >= _this.winTheshold) {
+          if (!_this.winTimeout) {
+            _this.winTimeout = window.setTimeout(function() {
+              _this.win();
+            }, _this.winTime);
+          }
+        } else {
+          window.clearTimeout(_this.winTimeout);
+          _this.winTimeout = false;
+        }
+
       }, function(err) {
         console.log("recordAudio():Audio Error: "+ err.code);
         console.log(err);
+        WalkingError.unsupported('microphone access');
       });
     }
 
-    requestAnimationFrame(_this.draw.bind(_this));
+    if (!_this.paused) {
+      requestAnimationFrame(_this.draw.bind(_this));
+    }
   },
+
+  win: function() {
+    var _this = this;
+
+    window.clearTimeout(_this.winTimeout);
+    window.clearTimeout(_this.failTimeout);
+
+    _this.paused = true;
+    _this.mediaRec.stopRecord();
+
+    var score = Game.getStepsPot();
+
+    Utilities.Dialog.read(_this.winDialog, function() {
+
+      Game.gameComplete(score);
+
+    });
+
+  },
+
+  fail: function() {
+    var _this = this;
+
+    window.clearTimeout(_this.winTimeout);
+
+    _this.paused = true;
+    _this.mediaRec.stopRecord();
+
+    Game.gameFail(function() {
+
+      Utilities.Dialog.read(_this.tryAgainDialog, function() {
+
+        _this.startGame();
+
+      });
+
+    }, function() {
+
+      Utilities.Dialog.read(_this.loseDialog, function() {
+
+        _this.$blackout.animate({'opacity': 0,}, 1000, 'linear', function() {
+          Router.go('/pages/compass/');
+        });
+
+      });
+
+    });
+
+  }
 };
 
+/*
 window.requestAnimationFrame = (function(){
   return  window.requestAnimationFrame ||
   window.webkitRequestAnimationFrame ||
@@ -58,6 +161,7 @@ window.requestAnimationFrame = (function(){
 })();
 
 var requestAnimationFrame = window.requestAnimationFrame;
+*/
 
 document.addEventListener('deviceready', function() {
   cordova.plugins.diagnostic.getMicrophoneAuthorizationStatus(function(status) {
