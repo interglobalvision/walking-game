aceb2bebb7ad1de500d608ae93166caa8c98acef
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-05-11 17:49:13 -0500

    add steps/points explanation to map intro dialog

--- cordova/www/pages/compass/compass.js
+++ cordova/www/pages/compass/compass.js
@@ -4,6 +4,7 @@ Home = {
       "And it's time to go on our first walk...",
       "Follow your compass, and find the golden flag...",
       "Each time you reach a flag - I will give you a MINIGAME CHALLENGE!!!",
+      "If you beat my minigame challenge - I will let you keep the steps you took to get there...",
       "Complete 3 minigame challenges to move to the next world...AND gain a new rank!!",
       "To begin you are ... " + Game.getRank() + " in the " + Game.getWorldName() + ".",
       "If you get lost on your way, just tap me at the bottom right...",
